e32a7d990bc73d7b93355cf38876425070e49ef0
feature <feature@feature6.andrew.cmu.edu>
2020-06-25 14:13:18 -0400

    test

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import datetime
 from read_repos import get_all_repos
 from get_commits import get_commits, parse_file_commits
 import csv
+from github import Github
+import pygit2
 
 
 if __name__ == "__main__":
@@ -11,9 +13,9 @@ if __name__ == "__main__":
 
     #get all jupyter notebooks from start date to end date, increment date
 
-    print('Getting repos...')
-    get_all_repos(datetime.datetime(2019, 2, 3), datetime.datetime(2019, 6, 1), 6, True)
-    print('done!')
+    # print('Getting repos...')
+    # get_all_repos(datetime.datetime(2019, 2, 3), datetime.datetime(2019, 6, 1), 6, True)
+    # print('done!')
 
 
     #get all notebooks in csv file by keyword (passed into get_commits function)
@@ -27,6 +29,10 @@ if __name__ == "__main__":
     # print('Getting Commits...')
     # parse_file_commits()
     # print('done!')
+
+
+repoClone = pygit2.clone_repository(github.com/hdong3030/GitHubAPI-Crawler, '.')
+    
 
 
 
